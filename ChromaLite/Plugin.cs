3c9c947cb0a1d11c3d3037101c456a504389dd21
kaitlyn~ <ItsKaitlyn03@protonmail.com>
2022-06-06 20:06:45 -0400

    Use Costura.Fody for 0Harmony

--- ChromaLite/Plugin.cs
+++ ChromaLite/Plugin.cs
@@ -5,10 +5,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace ChromaLite {
-    public class Plugin : IPlugin {
-
-        public string Name => "ChromaLite";
-        public string Version => "1.0.2";
+    public class Plugin {
 
         public static bool CTInstalled = false;
 
@@ -41,22 +38,6 @@ namespace ChromaLite {
 
         public void OnApplicationQuit() {
             SceneManager.sceneLoaded -= SceneManager_sceneLoaded;
-        }
-
-        public void OnLevelWasLoaded(int level) {
-
-        }
-
-        public void OnLevelWasInitialized(int level) {
-
-        }
-
-        public void OnUpdate() {
-
-        }
-
-        public void OnFixedUpdate() {
-
         }
 
         public static bool ChromaToggleInstalled() {
---
+++ ChromaLite/PluginWrapper.cs
@@ -0,0 +1,43 @@
+﻿using IllusionPlugin;
+
+namespace ChromaLite {
+    public class PluginWrapper : IPlugin {
+
+        public string Name => "ChromaLite";
+        public string Version => "1.0.2";
+
+        public Plugin PluginInstance;
+
+        public void OnApplicationStart() {
+
+            CosturaUtility.Initialize();
+            InitializePlugin();
+        }
+
+        private void InitializePlugin() {
+            PluginInstance = new Plugin();
+            PluginInstance.OnApplicationStart();
+        }
+
+        public void OnApplicationQuit() {
+            PluginInstance.OnApplicationQuit();
+        }
+
+        public void OnLevelWasLoaded(int level) {
+
+        }
+
+        public void OnLevelWasInitialized(int level) {
+
+        }
+
+        public void OnUpdate() {
+
+        }
+
+        public void OnFixedUpdate() {
+
+        }
+
+    }
+}
